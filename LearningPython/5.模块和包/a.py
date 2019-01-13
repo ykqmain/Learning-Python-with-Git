a445e3022fab6b3014e51fd55235e0461c0edd07
Yang <i@ykqmain.net>
2019-01-13 18:18:18 +0800

    LearningPython 上册

---
+++ LearningPython/5.模块和包/a.py
@@ -0,0 +1,15 @@
+import b
+import c
+import sys
+
+b.func('abc')
+
+print(b.str_b)
+
+print(c.mi(3, 4))
+
+print(b.str_bc)
+
+
+print(sys.path)
+
---
+++ LearningPython/5.模块和包/x.py
@@ -0,0 +1,8 @@
+from z import x, y
+
+x = 20
+
+y[0] = 5
+
+print(x, y)
+
